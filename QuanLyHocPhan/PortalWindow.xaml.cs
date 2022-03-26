c28402f082319712d120f0630bf34ae05909cae0
Tin Nguyen <hopthucuatin@gmail.com>
2022-03-26 19:36:13 +0700

    Initial commit

---
+++ QuanLyHocPhan/PortalWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace QuanLyHocPhan
+{
+    /// <summary>
+    /// Interaction logic for PortalWindow.xaml
+    /// </summary>
+    public partial class PortalWindow : Window
+    {
+        private void btnDanhSachNguoiDung_Click(object sender, RoutedEventArgs e) {
+            Main.Content = new UserList();
+        } 
+
+        private void btnDanhSachRole_Click(object sender, RoutedEventArgs e) {
+            Main.Content = new RoleList();
+        }
+
+        private void btnThongTinNhom_Click(object sender, RoutedEventArgs e) {
+            Main.Content = new GroupInfo();
+        }
+
+        public PortalWindow()
+        {
+            InitializeComponent();
+
+            Main.Content = new UserList();
+
+            
+        }
+
+    }
+
+    //public class MenuItem {
+    //    private String _name;
+    //    private Page _page;
+
+    //    public MenuItem(String name, Page page)
+    //    {
+    //        _name = name;
+    //        _page = page;
+    //    }
+    //}
+}
---
+++ QuanLyHocPhan/UserList.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace QuanLyHocPhan
+{
+    public class User
+    {
+        public String Username { get; set; }
+
+        // custom get roles return string
+        public string Roles
+        {
+            set; get;
+        }
+        public bool IsChecked { get; set; }
+    }
+
+
+    /// <summary>
+    /// Interaction logic for UserList.xaml
+    /// </summary>
+    public partial class UserList : Page
+    {
+        public int CurrentPage { get; set; }
+        public int TotalPage { get; set; }
+
+
+        public void btnCheckInfo_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        public void btnGrantRole_Click(object sender, RoutedEventArgs e)
+        {
+            // loop through all checkboxes, if checked, add to list
+            // List<User> list = new List<User>();
+            // foreach (User user in UserDataGrid.Items)
+            // {
+            //     if (user.IsChecked)
+            //     {
+            //         list.Add(user);
+            //     }
+            // }
+
+            // // get each roles
+            // string[] roles = new string[list.Count];
+            // for (int i = 0; i < list.Count; i++)
+            // {
+            //     roles[i] = list[i].GetRole();
+            // }
+
+            // // alert roles plat
+            // MessageBox.Show(string.Join(",", roles));
+        }
+
+        public UserList()
+        {
+            InitializeComponent();
+
+            // Load data
+            UserDataGrid.ItemsSource = new List<User>()
+            {
+                new User() { Username = "admin", Roles = "admin,user", IsChecked = false },
+                new User() { Username = "trungtin", Roles = "admin", IsChecked = false }
+            };
+
+        }
+    }
+}
